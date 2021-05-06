49675c84365dd029c63d587f9a4244ceeef0874e
Mihir Kapur <mihirkapur96@gmail.com>
2021-05-05 22:49:25 -0700

    changes

--- index.js
+++ index.js
@@ -100,3 +100,121 @@ app.get("/customer", async (req, res) => {
             });
         });
 });
+
+// GET /edit/5
+app.get("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM customer WHERE cusId = $1";
+    pool.query(sql, [id], (err, result) => {
+      // if (err) ...
+      res.render("edit", { model: result.rows[0] });
+    });
+  });
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const customer = [req.body.cusId, req.body.cusFname, req.body.cusLname,req.body.cusState,req.body.cusSalesYTD,req.body.cusSalesPrev];
+    const sql = "UPDATE customer SET ID = $1, FName = $2, LName = $3, State = $4, SalesYTD = $5, PrevYearSales = #6 WHERE (cusId = $1)";
+    pool.query(sql, customer, (err, result) => {
+      // if (err) ...
+      res.redirect("/customer");
+    });
+  });
+
+  // GET /delete/5
+app.get("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM customer WHERE cusId = $1";
+    pool.query(sql, [id], (err, result) => {
+      // if (err) ...
+      res.render("delete", { model: result.rows[0] });
+    });
+  });
+
+// POST /delete/5
+app.post("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const customer = [req.body.cusId, req.body.cusFname, req.body.cusLname,req.body.cusState,req.body.cusSalesYTD,req.body.cusSalesPrev];
+    const sql = "UPDATE customer SET ID = $1, FName = $2, LName = $3, State = $4, SalesYTD = $5, PrevYearSales = #6 WHERE (cusId = $1)";
+    pool.query(sql, customer, (err, result) => {
+      // if (err) ...
+      res.redirect("/customer");
+    });
+  });
+
+// GET /delete/5
+app.get("/Create Customer/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM customer WHERE cusId = $1";
+  pool.query(sql, [id], (err, result) => {
+    // if (err) ...
+    res.render("Create Customer", { model: result.rows[0] });
+  });
+});
+
+// POST /delete/5
+app.post("/Create Customer/:id", (req, res) => {
+  const id = req.params.id;
+  const customer = [req.body.cusId, req.body.cusFname, req.body.cusLname,req.body.cusState,req.body.cusSalesYTD,req.body.cusSalesPrev];
+  const sql = "UPDATE customer SET ID = $1, FName = $2, LName = $3, State = $4, SalesYTD = $5, PrevYearSales = #6 WHERE (cusId = $1)";
+  pool.query(sql, customer, (err, result) => {
+    // if (err) ...
+    res.redirect("/customer");
+  });
+});
+
+app.get("/import", (req, res) => {
+  res.render("import");
+});
+
+app.post("/import",  upload.single('Input1'), (req, res) => {
+   if(!req.file || Object.keys(req.file).length === 0) {
+       message = "Error: Import file not uploaded";
+       return res.send(message);
+   };
+   //Read file line by line, inserting records
+   const buffer = req.file.buffer; 
+   const lines = buffer.toString().split(/\r?\n/);
+
+   lines.forEach(line => {
+        //console.log(line);
+        customer = line.split(",");
+        //console.log(product);
+        const sql = "INSERT INTO customer(ID, Fname, Lname, State, SalesYTD, PrevYearSales) VALUES ($1, $2, $3, $4, $5, $6)";
+        pool.query(sql, customer, (err, result) => {
+            if (err) {
+                console.log(`Insert Error.  Error message: ${err.message}`);
+            } else {
+                console.log(`Inserted successfully`);
+            }
+       });
+   });
+   message = `Processing Complete - Processed ${lines.length} records`;
+   res.send(message);
+});
+
+app.get("/output", (req, res) => {
+  var message = "";
+  res.render("output",{ message: message });
+ });
+ 
+ 
+ app.post("/output", (req, res) => {
+     const sql = "SELECT * FROM customer ORDER BY cusId";
+     pool.query(sql, [], (err, result) => {
+         var message = "";
+         if(err) {
+             message = `Error - ${err.message}`;
+             res.render("output", { message: message })
+         } else {
+             var output = "";
+             result.rows.forEach(customer => {
+                 output += `${customer.cusid},${customer.cusfname},${customer.cuslname},${customer.cusstate}, ${customer.cussalesytd}, ${customer.cussalesprev}\r\n`;
+             });
+             res.header("Content-Type", "text/csv");
+             res.attachment("export.csv");
+             return res.send(output);
+         };
+     });
+ });
